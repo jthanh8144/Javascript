67299716b2f840cb720e50b58b613266cc884c5f
jthanh8144 <thanhvo618@gmail.com>
2021-06-16 23:45:03 +0700

    form validation 4

--- F8/FormValidation/main.js
+++ F8/FormValidation/main.js
@@ -58,10 +58,23 @@ function Validator(options) {
                     var formValues = Array.from(enableInputs).reduce(function (values, input) {
                         switch (input.type) {
                             case 'radio':
+                                values[input.name] = formElement.querySelector('input[name="' + input.name + '"]:checked').value;
+                                break;
                             case 'checkbox':
-
+                                if (!input.matches(':checked')) {
+                                    values[input.name] = '';
+                                    return values;
+                                }
+                                if (!Array.isArray(values[input.name])) {
+                                    values[input.name] = [];
+                                }
+                                values[input.name].push(input.value);
+                                break;
+                            case 'file':
+                                values[input.name] = input.files;
+                                break;
                             default:
-                                values[input.name] = input.value
+                                values[input.name] = input.value;
                         }
                         return values;
                     }, {});
@@ -152,7 +165,10 @@ Validator({
         Validator.isConfirmed('#re-password', function() {
             return document.querySelector('#form-1 #password').value;
         }, 'Mật khẩu'),
-        Validator.isRequired('input[type="radio"]', 'Vui lòng chọn giới tính!')
+        Validator.isRequired('avatar', 'Vui lòng chọn một file'),
+        Validator.isRequired('select', 'Vui lòng chọn khu vực!'),
+        Validator.isRequired('input[type="radio"]', 'Vui lòng chọn giới tính!'),
+        Validator.isRequired('input[type="checkbox"]', 'Vui lòng chọn ít nhất một sở thích!')
     ],
     onsubmit: function(data) {
         // Call API
