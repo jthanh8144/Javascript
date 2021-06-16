992c481f721ce13758fd26e1a94cb5af80b469cb
jthanh8144 <thanhvo618@gmail.com>
2021-06-16 16:01:26 +0700

    form validation 2

--- F8/FormValidation/main.js
+++ F8/FormValidation/main.js
@@ -1,20 +1,64 @@
 function Validator(options) {
+    var selectorRules = {};
+
     function Validate(inputElement, rule){
-        var errorMessage = rule.test(inputElement.value);
+        var errorMessage;
         var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
-                    if (errorMessage) {
-                        errorElement.innerText = errorMessage;
-                        inputElement.parentElement.classList.add('invalid');
-                    }
-                    else {
-                        errorElement.innerText = '';
-                        inputElement.parentElement.classList.remove('invalid');
-                    }
+        var rules = selectorRules[rule.selector];
+
+        for (var i = 0; i < rules.length; i++) {
+            errorMessage =  rules[i](inputElement.value);
+            if (errorMessage) break;
+        }
+        if (errorMessage) {
+            errorElement.innerText = errorMessage;
+            inputElement.parentElement.classList.add('invalid');
+        }
+        else {
+            errorElement.innerText = '';
+            inputElement.parentElement.classList.remove('invalid');
+        }
+        return !errorMessage;
     }
 
     var formElement = document.querySelector(options.form);
     if (formElement) {
+        formElement.onsubmit = function(e) {
+            e.preventDefault();
+
+            var isFormValid = true;
+
+            options.rules.forEach(function(rule) {
+                var inputElement = formElement.querySelector(rule.selector);
+                var isValid = Validate(inputElement, rule);
+                if (!isValid) {
+                    isFormValid = false;
+                }
+            });
+            if (isFormValid) {
+                // Submit với js
+                if (typeof options.onsubmit === 'function') {
+                    var enableInputs = formElement.querySelector('[name]:not([disabled])');
+                    var formValues = Array.from(enableInputs).reduce(function (values, input) {
+                        return (values[input.name] = input.value) && values;
+                    }, {});
+                    options.onsubmit(formValues);
+                }
+                // Submit với html
+                else {
+                    // formElement.submit();
+                }
+            } else {
+                
+            }
+        }
+
         options.rules.forEach(function(rule) {
+            if (Array.isArray(selectorRules[rule.selector])) {
+                selectorRules[rule.selector].push(rule.test);
+            } else {
+                selectorRules[rule.selector] = [rule.test];
+            }
             var inputElement = formElement.querySelector(rule.selector);
             var errorElement = inputElement.parentElement.querySelector('.form-message');
             if (inputElement) {
@@ -34,11 +78,11 @@ function Validator(options) {
 }
 
 // rule
-Validator.isRequired = function(selector) {
+Validator.isRequired = function(selector, message) {
     return {
         selector: selector,
         test: function(value) {
-            return value.trim() ? undefined : 'Vui lòng nhập trường này!';
+            return value.trim() ? undefined : message || 'Vui lòng nhập trường này!';
         }
     };
 }
@@ -53,24 +97,41 @@ Validator.isEmail = function(selector) {
     };
 }
 
-Validator.minLength = function(selector, min) {
+Validator.minLength = function(selector, min, message) {
     return {
         selector: selector,
         test: function(value) {
-            return value.length >= min ? undefined : `Mật khẩu phải có tối thiểu ${min} kí tự`;
+            return value.length >= min ? undefined : `${message} phải có tối thiểu ${min} kí tự`;
         }
     };
 }
 
-
+Validator.isConfirmed = function(selector, getRePass, message) {
+    return {
+        selector: selector,
+        test: function(value) {
+            return value === getRePass() ? undefined : `${message} không khớp!`
+        }
+    }
+}
 
 // Config
 Validator({
     form: '#form-1',
     errorSelector: '.form-message',
     rules: [
-        Validator.isRequired('#fullname'),
+        Validator.isRequired('#fullname', 'Vui lòng nhập họ tên!'),
+        Validator.isRequired('#email'),
         Validator.isEmail('#email'),
-        Validator.minLength('#password', 6),
-    ]
+        Validator.isRequired('#password'),
+        Validator.minLength('#password', 6, 'Mật khẩu'),
+        Validator.isRequired('#re-password'),
+        Validator.isConfirmed('#re-password', function() {
+            return document.querySelector('#form-1 #password').value;
+        }, 'Mật khẩu')
+    ],
+    onsubmit: function(data) {
+        // Call API
+        console.log(data);
+    }
 });
