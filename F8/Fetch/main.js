ec5ad10ce1ec9e8cabd99619937789b1b912d282
jthanh8144 <thanhvo618@gmail.com>
2021-06-27 15:28:53 +0700

    Fetch

---
+++ F8/Fetch/main.js
@@ -0,0 +1,18 @@
+var postApi = 'https://jsonplaceholder.typicode.com/posts';
+fetch(postApi)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(posts) {
+        var htmls = posts.map(function(post) {
+            return `<li>
+                <h2>${post.title}</h2>
+                <p>${post.body}</p>
+            </li>`;
+        });
+        var html = htmls.join('');
+        document.getElementById('post-block').innerHTML = html;
+    })
+    .catch(function() {
+        console.log('Có lỗi');
+    });
