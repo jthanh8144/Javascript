df36ae75347e4b0a4c25f9fb8ff44aa69c0f32c7
jthanh8144 <thanhvo618@gmail.com>
2021-06-29 13:11:35 +0700

    tabs

---
+++ F8/Tabs/main.js
@@ -0,0 +1,20 @@
+var tabs = document.querySelectorAll('.tab-item');
+var panes = document.querySelectorAll('.tab-pane');
+var tabActive = document.querySelector('.tab-item.active');
+var line = document.querySelector('.line');
+
+line.style.left = tabActive.offsetLeft + 'px';
+line.style.width = tabActive.offsetWidth + 'px';
+
+tabs.forEach((tab, index) => {
+    const pane = panes[index];
+    tab.onclick = function() {
+        document.querySelector('.tab-item.active').classList.remove('active');
+        this.classList.add('active');
+        document.querySelector('.tab-pane.active').classList.remove('active');
+        pane.classList.add('active');
+
+        line.style.left = tab.offsetLeft + 'px';
+        line.style.width = tab.offsetWidth + 'px';
+    }
+})
