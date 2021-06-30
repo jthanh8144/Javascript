f1af581e59959be2464c9ee5701f8da04c6417c4
jthanh8144 <thanhvo618@gmail.com>
2021-06-30 18:30:23 +0700

    final

--- F8/MusicPlayer/main.js
+++ F8/MusicPlayer/main.js
@@ -1,6 +1,9 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+const PLAYER_STORAGE_KEY = 'PLAYER';
+
+const playlist = $('.playlist');
 const heading = $('header h2');
 const cd = $('.cd');
 const cdThumb = $('.cd-thumb');
@@ -10,10 +13,14 @@ const player = $('.player');
 const progress = $('#progress');
 const nextBtn = $('.btn-next');
 const preBtn = $('.btn-prev');
-
-var isPlaying = false;
+const randomBtn = $('.btn-random');
+const repeatBtn = $('.btn-repeat');
 
 const app = {
+    config: JSON.parse(localStorage.getItem(PLAYER_STORAGE_KEY)) || {},
+    isPlaying: false,
+    isRandom: false,
+    isRepeat: false,
     currentIndex: 0,
     songs: [
         {
@@ -53,10 +60,21 @@ const app = {
             image: "https://i.scdn.co/image/ab67616d0000b273d1b5e4b7be8a727c73bfcc53"
         }
     ],
+    setConfig: function(key, value) {
+        this.config[key] = value;
+        localStorage.setItem(PLAYER_STORAGE_KEY, JSON.stringify(this.config));
+    },
+    loadConfig: function() {
+        this.isRandom = this.config.isRandom;
+        this.isRepeat = this.config.isRepeat;
+        
+        repeatBtn.classList.toggle('active', this.isRepeat);
+        randomBtn.classList.toggle('active', this.isRandom);
+    },
     render: function() {
-        const htmls = this.songs.map(song => {
+        const htmls = this.songs.map((song, index) => {
             return `
-                <div class="song">
+                <div class="song ${index === this.currentIndex ? 'active' : ''}" data-index="${index}">
                     <div class="thumb" style="background-image: url('${song.image}')"></div>
 
                     <div class="body">
@@ -69,14 +87,14 @@ const app = {
                     </div>
                 </div>`
         });
-        $('.playlist').innerHTML = htmls.join('');
+        playlist.innerHTML = htmls.join('');
     },
     defineProperties: function() {
         Object.defineProperty(this, 'currentSong', {
             get: function() {
                 return this.songs[this.currentIndex];
             }
-        })
+        });
     },
     handleEvent: function() {
         const _this = this;
@@ -130,12 +148,59 @@ const app = {
         }
 
         nextBtn.onclick = () => {
-            _this.nextSong();
+            if (_this.isRandom) {
+                _this.playRandomSong();
+            } else {
+                _this.nextSong();
+            }
             audio.play();
+            _this.render();
+            _this.scrollToActiveSong();
         }
         preBtn.onclick = () => {
-            _this.preSong();
+            if (_this.isRandom) {
+                _this.playRandomSong();
+            } else {
+                _this.preSong();
+            }
             audio.play();
+            _this.render();
+            _this.scrollToActiveSong();
+        }
+
+        randomBtn.onclick = () => {
+            _this.isRandom = !_this.isRandom;
+            _this.setConfig('isRandom', _this.isRandom);
+            randomBtn.classList.toggle('active', _this.isRandom);
+        }
+
+        audio.onended = () => {
+            if (this.isRepeat) {
+                audio.play();
+            } else {
+                nextBtn.click();
+            }
+        }
+
+        repeatBtn.onclick = () => {
+            _this.isRepeat = !_this.isRepeat;
+            _this.setConfig('isRepeat', _this.isRepeat);
+            repeatBtn.classList.toggle('active', _this.isRepeat);
+        }
+
+        playlist.onclick = (e) => {
+            const songElement = e.target.closest('.song:not(.active)');
+            if (songElement || e.target.closest('.option')) {
+                if (songElement && !e.target.closest('.option')) {
+                    _this.currentIndex = Number(songElement.dataset.index);
+                    _this.loadCurrentSong();
+                    _this.render();
+                    audio.play();
+                }
+                if (e.target.closest('.option')) {
+                    // xử lí
+                }
+            }
         }
     },
     loadCurrentSong: function() {
@@ -157,7 +222,25 @@ const app = {
         }
         this.loadCurrentSong();
     },
+    playRandomSong: function() {
+        var newIndex;
+        do {
+            newIndex = Math.floor(Math.random() * app.songs.length)
+        } while (this.currentIndex === newIndex);
+        this.currentIndex = newIndex;
+        this.loadCurrentSong();
+    },
+    scrollToActiveSong: function() {
+        setTimeout(() => {
+            $('.song.active').scrollIntoView({
+                behavior: 'smooth',
+                block: 'nearest'
+            });
+        }, 100);
+    },
     start: function() {
+        this.loadConfig();
+
         this.defineProperties();
         this.handleEvent();
 
