9409352cf6f9e3abcc789e1cd815afee0da54275
jthanh8144 <thanhvo618@gmail.com>
2021-07-31 20:49:56 +0700

    this keyword

--- Advance/main.js
+++ Advance/main.js
@@ -141,6 +141,78 @@ console.log(sum(6, 9));*/
 }
 console.log(sum(1, 2));*/
 
-'use strict';
+/*'use strict';
 const private = 123;
-console.log(private);
+console.log(private);*/
+
+/* this keyword */
+/*const pixel1 = {
+    name: 'Google pixel',
+    color: 'Really blue',
+    weight: 300,
+
+    takePhoto() {
+        console.log(this);
+    },
+
+    objChild: {
+        name: 'Child Object',
+        methodChild() {
+            console.log(this);
+        }
+    }
+};
+pixel1.objChild.methodChild();*/
+
+/*function Car(name, color, weight) {
+    this.name = name;
+    this.color = color;
+    this.weight = weight;
+    this.run = function() {
+        console.log('running...', this);
+    }
+}
+
+const mercedesBenZ = new Car('Mercedes Ben Z', 'Black', 5000);
+console.log(mercedesBenZ.run());*/
+
+/*const button = document.querySelector('button');
+button.onclick = function() {
+    console.dir(this.innerText);
+}*/
+
+/*function myFunc() {
+    console.log(this);
+}
+myFunc();*/
+/*'use strict';
+function myFunc() {
+    console.log(this);
+}
+myFunc();*/
+
+
+'use strict';
+function Car(name, color) {
+    this.name = name;
+    this.color = color;
+    // this.run = function() {
+    //     console.log(this);
+    // }
+}
+Car.prototype.run = function() {
+    // console.log(this);
+
+    // function test() {
+    //     console.log(this);
+    // }
+
+    const test = () => {
+        console.log(this);
+    }
+    test();
+}
+const mercedesBenZ = new Car('Mercedes Ben Z', 'Black');
+const hondaCRV = new Car('Honda CRV', 'White');
+console.log(mercedesBenZ.run());
+console.log(hondaCRV.run());
