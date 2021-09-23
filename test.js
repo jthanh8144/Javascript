29cc2a017fd6a200eabe14d5910f91420b2255e4
VoVanThanh <thanhvo618@gmail.com>
2021-09-23 17:10:12 +0700

    fix css form validation

--- test.js
+++ test.js
@@ -1,21 +1,32 @@
-var a = 1;
-var b = 4;
-var n = 2345;
-
-function run() {
-    do {
-        if (n % a == 0) {
-            if (n == a || n == b + 1) {
-                console.log(true);
-                return;
-            } 
-            else n /= a;
-        } else {
-            n -= b;
-        }
-    } while (n != 1 && n > 0);
-    if (n == 1) console.log(true);
-    else console.log(false);
+var a = [
+    44299, 
+    44299, 
+    46145, 
+    46145, 
+    46145, 
+    46145, 
+    46145, 
+    47991, 
+    47991, 
+    47991, 
+    47991, 
+    49837, 
+    49837, 
+    49837, 
+    49837, 
+    49837, 
+    49837, 
+    51682, 
+    51682, 
+    51682, 
+    51682, 
+    51682, 
+    51682,
+];
+var s = 0;
+for (var i = 0; i < a.length; i++) {
+    s += a[i];
 }
-
-run();
+s -= 115000;
+s += 11500;
+console.log(s);
